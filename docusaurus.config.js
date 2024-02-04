d99fda602c669588a7c0ead46fe2718a1fce743f
yuseon.lim <dogakday@gmail.com>
2024-02-04 14:07:08 +0900

    feat: social card 설정

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -50,7 +50,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: "img/docusaurus-social-card.jpg",
+      image: "img/introduce.png",
       navbar: {
         title: "뉴뉴의 메모장",
         logo: {
